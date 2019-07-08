21fe9a7da31dfe19188c43798eb6304c85dca2d8
Paul Roggeveen <paulroggeveen@hotmail.com>
2019-07-08 21:20:17 +0200

    add GET method

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 
-@app.route("/", methods = ['POST'])
+@app.route("/", methods = ['GET', 'POST'])
 def predict():
-
-    input_data = request.get_json()
-
-    return '{"score":' + str(2 * float(input_data['data'])) + '}'
-
+	if request.method == 'POST':
+        input_data = request.get_json()
+        return '{"score":' + str(2 * float(input_data['data'])) + '}'
+    else:
+        return ''
 if __name__ == "__main__":
     app.run()
